e86a698bc5437871b3e2a04b617ff7c4ab5b226f
unknown <camila.domingos@al.infnet.edu.br>
2022-08-26 22:01:09 -0300

    request

--- my-app/src/Views/Home/index.tsx
+++ my-app/src/Views/Home/index.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
-import { toast } from "react-toastify";
 import { getProducts } from "../../services/product";
 import { Layout } from "../../components/Layout";
 import { product } from "../../entities/Products";
@@ -10,23 +9,18 @@ import { Card, Row, Col } from "react-bootstrap";
 import { Loading } from "../../components/Loading";
 import { addToCart } from "../../store/slices/cardSlice"
 
-type productsProps = product | null
-
 export function HomeView () {
-    const [Products, setProducts]:any = useState()
-        useEffect(() => {
-            const fetch = async ():Promise<void | productsProps > => {
-                try{
-                    const result = await getProducts()
-                    setProducts(result)
-               } catch{
-                    toast.error('Erro ao carregar produtos. Tente novamente', {
-                        theme: 'colored'
-                    })
-               }      
-            }
-            fetch()
-        }, [])
+    const [products]:any = useState()
+
+    async function pegprodutos() {
+            const result = await getProducts();
+            console.log(result)
+    } 
+
+    useEffect(() => {
+        pegprodutos();
+    }, []);
+
         const dispatch = useDispatch()
     const navigate = useNavigate()
         const handleAddToCart = (item:product) => {
@@ -36,18 +30,19 @@ export function HomeView () {
     return (
         <Layout>
              <Container>
-                {!Products ? (
+                {!products ? (
                     <Loading />
                 ) : (
                     <>
                       <Row>
-                        {Products.map((item:product) => (
+                        {products.map((item:product) => (
                             <Col key={item.id} className='mb-4' xs={6} md={4} lg={3}>
                             <Card className='text-center shadow h-100' key={item.name}>
                                 <Card.Img variant="top" src={item.image} alt={item.name} width={220} height={220}/>
                                 <Card.Body className='d-flex flex-column align-items-center'>
                                 <Card.Title as='h2' className='h5'>{item.name}</Card.Title>
                                 <Card.Text>{item.description}</Card.Text>
+                                <Card.Text>{item.brand}</Card.Text>
                                 <Card.Text as='h5'>{item.price}</Card.Text>
                                 <Button onClick={() => handleAddToCart(item)}>Adicionar ao carrinho</Button>
                                 </Card.Body>
--- my-app/src/services/product.ts
+++ my-app/src/services/product.ts
@@ -1,18 +1,5 @@
-import { product } from "../entities/Products"
-
-export const getProducts = async ():Promise<product[]> => {
-  const response = await fetch(`https://mks-frontend-challenge-api.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC`)
-  const products = await getProducts()
-  const productsSnapshot = products.response.map(response => ({...response.data(),id:response.id}))
-  return productsSnapshot as product[]   
-}
-
-export const getProductsById = async (id:string):Promise<product> => {
-  const docProduct = response(`https://mks-frontend-challenge-api.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC`)
-  const productSnapshot = await getProducts(responseProduct)
-  if (!productSnapshot.exists()) {
-      throw new Error('Produto não encontrado.')
-  }
-  const product = productSnapshot.data()
-  return {id, ...product} as product
+export const getProducts = async () => {
+  const response = await fetch(`https://mks-frontend-challenge-api.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC`);
+  const json = await response.json();
+  return json;
 }
